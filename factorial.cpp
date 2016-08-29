34481103fff131743ee981ff735c4305b3a16234
Sahil Arora <sahilarora.535@gmail.com>
2016-08-29 15:57:16 +0530

    Add check for negative numbers in factorial.cpp(Q2)

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,10 @@
 #include "functions.h"
 
 int factorial(int n){
+	while(n<0){
+		std::cout<<"\nInvalid value of n. Please re-enter: ";
+		std::cin>>n;
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
