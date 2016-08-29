62fd04efdf098311608ad94ef4a2b22e603dffaf
Sahil Arora <sahilarora.535@gmail.com>
2016-08-29 16:06:17 +0530

    Convert factorial from recursive to iterative

--- factorial.cpp
+++ factorial.cpp
@@ -5,8 +5,11 @@ int factorial(int n){
 		std::cout<<"\nInvalid value of n. Please re-enter: ";
 		std::cin>>n;
 	}
-    if(n!=1){
-	return(n * factorial(n-1));
-    }
-    else return 1;
+ //    if(n!=1){
+	// return(n * factorial(n-1));
+ //    }
+ //    else return 1;
+	int ans = 1;
+	for(int i=1; i<=n; ++i) ans *= i;
+	return ans;
 }
